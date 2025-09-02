9f6802014b37b245485182537fa169498d855e4d
José Mateus <mateusjoseph@gmail.com>
2025-09-02 19:02:19 -0300

    primeira versão

---
+++ script.js
@@ -0,0 +1,34 @@
+let clientWeb = null;
+
+const clientId = "ESP8266" + Math.floor(Math.random() * 900) + 100;
+clientWeb = new Paho.MQTT.Client("broker.hivemq.com", 8884, clientId);
+
+clientWeb.connect({
+    useSSL: true,
+    timeout: 5,
+    onSuccess: function (){
+        alert("Conectado com sucesso!")
+    },
+    onFailure: function (){
+        alert("A conexão falhou!")
+    }
+});
+
+function ligarAmarelo(){
+    document.getElementById("amarelo").classList.add("amar");
+
+    //fazendo publish no tópico, (broker)
+    const msgAmar = new Paho.MQTT.Message("");
+    msgAmar.destinationName = "jmr/led/amarelo/on"
+    clientWeb.send(msgAmar)
+}
+
+function desligar(){
+    document.getElementById("amarelo").classList.remove("amar");
+    
+    
+    let msg = new Paho.MQTT.Message("");
+    msg.destinationName = "jmr/led/amarelo/off";
+    clientWeb.send(msg);     
+}
+
